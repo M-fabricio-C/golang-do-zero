fa9412b4feffb2ffce36bb462c9b9062ba35fc04
Marcos Fabricio Câmara Silva <166764241+M-fabricio-C@users.noreply.github.com>
2024-04-26 15:46:33 -0300

    if-else concluido o estudo

--- modulo-4/10-if-else.go
+++ modulo-4/10-if-else.go
@@ -7,11 +7,33 @@ import "fmt"
 
 func main() {
 
-	numero := 1
+//	numero := 1
 	// if (condição) { <ação> }
-	if numero == 2 {
-		fmt.Println("Valor é igual a 1")
-	} else{
-		fmt.Println("Valor não é igual a 1")
-	}
+//	if numero == 1 {
+//		fmt.Println("Valor é igual a 1")
+//	} else{
+//		fmt.Println("Valor não é igual a 1")
+//	}
+
+//    if numero == 1 {
+//		fmt.Println("Valor é igual a 1")
+//	} else if numero == 2 {
+//		fmt.Println("Valor é igual a 2")
+//	} else {
+//		fmt.Println("Valor é diferente de 1 e 2")
+//	}
+
+      numero := 8
+      if numero%2 == 0 {
+		fmt.Printf("%d é par", numero)
+	  } else {
+		fmt.Printf("%d é impar", numero)	  
+      }
+
+//		fmt.Println(1 + 1)
+//		fmt.Println(2 - 1)
+//		fmt.Println(2 * 2)
+//		fmt.Print(10 / 2)
+//		fmt.Println(10 % 2)
+
 }
