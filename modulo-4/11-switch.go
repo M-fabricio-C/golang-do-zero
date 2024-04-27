085fd6abe211b55fc7ca9ca04fd3798d88773350
Marcos Fabricio Câmara Silva <166764241+M-fabricio-C@users.noreply.github.com>
2024-04-27 15:40:27 -0300

    Fix concluida

---
+++ modulo-4/11-switch.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+//	posicao := 1
+//	switch posicao {
+//	case 1:
+//		fmt.Println("Primeiro lugar")
+//	case 2:
+//		fmt.Println("Segundo lugar")
+//	case 3:
+//		fmt.Println("Terceiro lugar")		
+//	}
+
+   nome := "tomas"
+   switch nome {
+   case "levi":
+	 fmt.Println("É uma pessoa")
+   case "tomas":
+	 fmt.Println("É um gato")
+   case "regi":
+	 fmt.Println("É uma menina")	 
+   }
+
+}
